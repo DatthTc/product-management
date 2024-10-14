2e6816ffbba11d4c7188d8834c7cea245b3d91fa
datt <162810439+DatthTc@users.noreply.github.com>
2024-10-14 15:09:39 +0700

    Cài mongodb -  mongoose

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const express = require("express");
 require("dotenv").config();
 
+const database = require("./config/database");
 const route = require("./routes/client/index.route");
+
+database.connect();
 
 const app = express();
 const port = process.env.PORT;
