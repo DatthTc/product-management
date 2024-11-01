a4bf5250f3fe6dfff9e398c1ba50e6c3ed5692c7
datt <162810439+DatthTc@users.noreply.github.com>
2024-11-01 14:52:56 +0700

    16. lấy data in ra giao diện

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,14 @@
-module.exports.index = (req, res) => {
-    res.render("admin/pages/products/index.pug", {
-        pageTitle: "Danh Sách Sản Phẩm ",
-      });
+const Product = require("../../models/product-model");
+//[GET] /admin/products
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false,
+  });
+  console.log(products);
+
+  res.render("admin/pages/products/index.pug", {
+    pageTitle: "Danh Sách Sản Phẩm ",
+    //truyen` data ra ngoai giao dien
+    products: products,
+  });
 };
