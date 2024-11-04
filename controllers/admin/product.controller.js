be5c1785ac84babb53aedc89bd4816e96a248f8f
datt <162810439+DatthTc@users.noreply.github.com>
2024-11-04 21:25:07 +0700

    Tính năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,14 +1,54 @@
 const Product = require("../../models/product-model");
 //[GET] /admin/products
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
+  // console.log(req.query.status);
+
+  let filterStatus = [
+    {
+      name: "Tất Cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt Động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng Hoạt Động",
+      status: "inactive",
+      class: "",
+    },
+  ];
+
+  if (req.query.status) {
+    const index = filterStatus.findIndex(
+      (item) => item.status == req.query.status
+    );
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex((item) => item.status == "");
+    filterStatus[index].class = "active";
+  }
+
+  // tại sao file js không viết luôn vào controller là vì : Controller tập trung vào logic xử lý yêu cầu và trả về kết quả. nên không thích hợp xử lý logic phức tạp
+  let find = {
     deleted: false,
-  });
-  console.log(products);
+  };
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  const products = await Product.find(find);
+
+  // console.log(products);
 
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Danh Sách Sản Phẩm ",
     //truyen` data ra ngoai giao dien
     products: products,
+    // truyền mảng fillterStatus ra ngoài giao diện
+    filterStatus: filterStatus,
   });
 };
---
+++ public/admin/js/script.js
@@ -0,0 +1,23 @@
+// button-status
+const buttonStatus = document.querySelectorAll("[button-status]");
+// new URL cần để dùng các hàm hỗ trợ như searchParam bên dưới
+const url = new URL(window.location.href);
+
+if (buttonStatus.length > 0) {
+  buttonStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      //getAttribute : để lấy giá trị của thuộc tính
+      const status = button.getAttribute("button-status");
+
+      if (status) {
+        url.searchParams.set("status", status);
+      } else {
+        url.searchParams.set("status");
+      }
+
+      window.location.href = url.href;
+    });
+  });
+}
+
+// end button-status
