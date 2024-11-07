6c15c155f15752784685baf5afed7bce675700ab
datt <162810439+DatthTc@users.noreply.github.com>
2024-11-08 00:32:35 +0700

    Tối ưu Chức Năng Tìm Kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,37 +1,14 @@
+// tại sao file js không viết luôn vào controller là vì : Controller tập trung vào logic xử lý yêu cầu và trả về kết quả. nên không thích hợp xử lý logic phức tạp
 const Product = require("../../models/product-model");
 //[GET] /admin/products
+
+const filterStatusHelpers = require("../../helpers/filterStatus.helper");
+const searchHelpers = require("../../helpers/searchProduct");
+
 module.exports.index = async (req, res) => {
-  // console.log(req.query.status);
+  // là hàm filterStatus chứa một chức năng từ helpers được truyền từ filterStatusHelpers
+  const filterStatus = filterStatusHelpers(req.query);
 
-  let filterStatus = [
-    {
-      name: "Tất Cả",
-      status: "",
-      class: "",
-    },
-    {
-      name: "Hoạt Động",
-      status: "active",
-      class: "",
-    },
-    {
-      name: "Dừng Hoạt Động",
-      status: "inactive",
-      class: "",
-    },
-  ];
-
-  if (req.query.status) {
-    const index = filterStatus.findIndex(
-      (item) => item.status == req.query.status
-    );
-    filterStatus[index].class = "active";
-  } else {
-    const index = filterStatus.findIndex((item) => item.status == "");
-    filterStatus[index].class = "active";
-  }
-
-  // tại sao file js không viết luôn vào controller là vì : Controller tập trung vào logic xử lý yêu cầu và trả về kết quả. nên không thích hợp xử lý logic phức tạp
   let find = {
     deleted: false,
   };
@@ -39,19 +16,12 @@ module.exports.index = async (req, res) => {
   if (req.query.status) {
     find.status = req.query.status;
   }
-  // chức năng function of search
-  let keyword = "";
-  if (req.query.keyword) {
-    keyword = req.query.keyword;
-
-    const regex = new RegExp(keyword, "i");
-    //title trong database lấy tên sp tạo một key title vào trong hàm find có giá trị = keyword mà ng dùng nhập vào
-    find.title = regex;
+  const objectSearh = searchHelpers(req.query);
+  if (objectSearh.keyword) {
+    find.title = objectSearh.regex;
   }
 
   const products = await Product.find(find);
-
-  // console.log(products);
 
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Danh Sách Sản Phẩm ",
@@ -59,6 +29,6 @@ module.exports.index = async (req, res) => {
     products: products,
     // truyền mảng fillterStatus ra ngoài giao diện
     filterStatus: filterStatus,
-    keyword: keyword,
+    keyword: objectSearh.keyword,
   });
 };
